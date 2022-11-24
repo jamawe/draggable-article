7592d9aee9a0cd809f4d3e5af6af4e21eea8fb96
Jana Wernick <jana.wernick@gmail.com>
2022-11-24 13:18:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+const templateBasic = [
+  { key: 'heading', value: '', id: generateId(), errors: { value: '' } },
+  { key: 'summary', value: '', id: generateId(), errors: { value: '' } },
+  { key: 'paragraph', value: '', id: generateId(), errors: { value: '' } },
+]
+
+function generateId() {
+  return Math.floor(Date.now() * Math.random());
+}
+
+const sectionsToSelect = [
+  'heading', 'image', 'listarray', 'paragraph', 'subheading', 'summary', 'table'
+];
+
+// Create deep template copy
+// NOTE: Can be skipped if template logic will not be implemented
+const articleForForm = JSON.parse(JSON.stringify(templateBasic));
+const articleForDisplay = []; // Derive from articleForForm
+</script>
+
+<template>
+  <div class="form">
+  </div>
+  
+  <!-- <div class="display">
+
+  </div> -->
+</template>
+
+<style scoped>
+</style>
