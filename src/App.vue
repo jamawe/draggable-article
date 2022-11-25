0c62bf1beb40695b605265b7014616b1ef48ef82
Jana Wernick <jana.wernick@gmail.com>
2022-11-25 18:03:23 +0100

    feature: Add basic image section logic + JSDoc comments for existing functions

--- src/App.vue
+++ src/App.vue
@@ -25,27 +25,54 @@ const state = ref({
   previousSelect: '',
 });
 
+// GENERAL
+/**
+ * Sets disabled props in objects of sectionsToSelect array to false based on unique sections already existing in articleForForm
+ */
 function disableSectionsBasedOnArticle() {
   articleForForm.value.forEach(section => {
     // If prev select is unique section, disable option in select
     if (state.value.uniqueSections.includes(section.name)) {
-      console.log('%c', 'color: darkseagreen; font-weight: bold;', section.name);
       const i = findIndexOfUniqueSection(section.name);
       setDisabledOnSectionsToSelect(i, true);
     } 
   });
 }
 
+/**
+ * Returns the index of a section in articleForForm by its id prop
+ * @param {number} sectionId The id of the section 
+ * @returns {number} The index of the section in articleForForm
+ */
+function findArticleSectionIndex(sectionId) {
+  return articleForForm.value.findIndex(section => section.id === sectionId);
+}
+
+/**
+ * Adds an empty section object to the articleForForm array
+ */
 function addSection() {
   const newSection = { name: '', id: generateId(), errors: { value: '' } };
   articleForForm.value.push(newSection);
 }
 
+// TODO Delete section
+
+// SELECT
+/**
+ * Save the previously selected option on the focus event of a select element.
+ * @param {string} previousSelect The previously selected option
+ */
 function setPreviousSelect(previousSelect) {
-  console.log('%cpreviousSelect', 'color: darkseagreen; font-weight: bold;', previousSelect);
   state.value.previousSelect = previousSelect;
 }
 
+/**
+ * Handler that manages the change of an option in the section select element.
+ * @param {string} newSelect The newly selected option
+ * @param {string} sectionId The id of the section
+ * @returns {undefined} Undefined (explicit) if the newly selected option is the same as the previously selected one or nothing
+ */
 function handleNewSelect(newSelect, sectionId) {
   // If prev + new select are the same, do nothing
   if (state.value.previousSelect === newSelect) return;
@@ -66,23 +93,36 @@ function handleNewSelect(newSelect, sectionId) {
   resetSectionAfterSelectChange(state.value.previousSelect, sectionId);
 }
 
+/**
+ * Returns the index of the uniqueSection in the sectionsToSelect array by its name prop
+ * @param {string} uniqueSection A section that only should appear once per article
+ * @returns {number} The index of the uniqueSection in the sectionsToSelect array
+ */
 function findIndexOfUniqueSection(uniqueSection) {
   const i = state.value.sectionsToSelect.findIndex(section => section.name === uniqueSection);
-  console.log('%cfindIndexOfUniqueSection i', 'color: darkseagreen; font-weight: bold;', i);
   return i;
 }
 
+/**
+ * Sets the disabled prop on an object of the sectionsToSelect array by its index
+ * @param {number} indexSection The index of objects in sectionsToSelect
+ * @param {boolean} isDisabled The value to which the disabled prop should be set
+ */
 function setDisabledOnSectionsToSelect(indexSection, isDisabled) {
   state.value.sectionsToSelect[indexSection].disabled = isDisabled;
 }
 
-function resetSectionAfterSelectChange(sectionToReset, sectionId) {
-  // Get index of sectionToReset in articleForForm
+/**
+ * Resets the props of a section according to its type after a new option was selected
+ * @param {string} sectionName The name (type) of the section
+ * @param {number} sectionId The id of the section
+ */
+function resetSectionAfterSelectChange(sectionName, sectionId) {
+  // Get index of sectionName in articleForForm
   const i = findArticleSectionIndex(sectionId);
-  console.log('%cresetSectionAfterSelectChange i', 'color: darkseagreen; font-weight: bold;', i);
 
-  // Reset props sectionToReset object
-  switch (sectionToReset) {
+  // Reset props sectionName object
+  switch (sectionName) {
     case 'image':
       articleForForm.value[i].url = '';
       articleForForm.value[i].name = '';
@@ -103,8 +143,48 @@ function resetSectionAfterSelectChange(sectionToReset, sectionId) {
   }
 }
 
-function findArticleSectionIndex(sectionId) {
-  return articleForForm.value.findIndex(section => section.id === sectionId);
+// IMAGE SECTION
+/**
+ * Checks the file for its correct type and sets the img element's src attribute of the respective image section
+ * @param {array} filesArray The files array of a file input event
+ * @param {number} sectionId The id of the section
+ */
+function handleImage(filesArray, sectionId) {
+  // Only one image can be selected for now
+  const [file] = filesArray;
+
+  // Handle cancelled upload
+  if (!file) return;
+  // Validate file type
+  if (!file.type.includes('image')) return;
+
+  // Select DOM elements relevant to display image upload progress
+  const imagePreview = document.querySelector(`#imagePreview-${sectionId}`);
+  imagePreview.src = URL.createObjectURL(file);
+}
+
+/**
+ * Handler for checking/unchecking is_title_image checkboxes depending on whether a title image already exists in the article
+ * @param {boolean} isChecked The checkbox input value
+ * @param {number} sectionId The id of the section
+ */
+function handleCheckboxAction(isChecked, sectionId) {
+  // 1. Get all titleImage checkboxes + convert NodeList to array with spread operator
+  // 2. Filter checkboxes that don't have the current sectionId
+  const otherCheckboxes = [...getAllTitleImageCheckboxes()].filter(({ id }) => id !== `imageTitleImage-${sectionId}`);
+
+  // Disable other image checkboxes if one is checked (true)
+  if (isChecked) otherCheckboxes.forEach(checkbox => checkbox.disabled = true);
+  // Enable other image checkboxes if one is unchecked (false)
+  if (!isChecked) otherCheckboxes.forEach(checkbox => checkbox.disabled = false);
+}
+
+/**
+ * Returns a NodeList of all checkboxes that start with the id 'imageTitleImage'
+ * @returns {NodeList} A NodeList of checkbox input fields
+ */
+function getAllTitleImageCheckboxes() {
+  return document.querySelectorAll(`[id^="imageTitleImage"]`);
 }
 
 // Create deep template copy
@@ -126,7 +206,7 @@ disableSectionsBasedOnArticle();
         <div>
           <!-- SELECT SECTION -->
           <div>
-            <label for="">Section:</label>
+            <label for="">Section</label>
             <select
               v-model="element.name" name="" id="" 
               @focus="setPreviousSelect($event.target.value)"
@@ -151,23 +231,47 @@ disableSectionsBasedOnArticle();
           <template v-else-if="element.name === 'image'">
             <!-- Button to choose image -->
             <div>
-              <label for="">
+              <label :for="`imageFile_${element.id}`">
                 <span>Choose Image</span>
               </label>
-              <input type="file" name="" id="" accept="image/*">
+              <input
+                @change="handleImage($event.target.files, element.id)"
+                type="file"
+                :name="`imageFile_${element.id}`"
+                :id="`imageFile_${element.id}`"
+                accept="image/*">
             </div>
 
             <!-- Preview with progress bar -->
-            <div></div>
+            <div>
+              <img src="" :id="`imagePreview-${element.id}`" style="max-height: 150px;object-fit: contain;">
+            </div>
 
             <!-- Name -->
-            <div></div>
+            <!-- <div></div> -->
 
             <!-- Alt text -->
-            <div></div>
+            <div>
+              <label :for="`imageAlt-${element.id}`">Alternative text</label>
+              <textarea
+                v-model.trim="element.alt"
+                :name="`imageAlt-${element.id}`"
+                :id="`imageAlt-${element.id}`"
+                cols="30" rows="2"></textarea>
+            </div>
 
             <!-- Title image -->
-            <div></div>
+            <div>
+              <label :for="`imageTitleImage-${element.id}`">
+                <span>Title image</span>
+                <input
+                  v-model="element.is_title_image"
+                  @change="handleCheckboxAction($event.target._modelValue, element.id)"
+                  type="checkbox"
+                  :name="`imageTitleImage-${element.id}`"
+                  :id="`imageTitleImage-${element.id}`">
+              </label>
+            </div>
           </template>
 
           <!-- LISTARRAY SECTION -->
@@ -193,7 +297,7 @@ disableSectionsBasedOnArticle();
           <!-- HEADING / PARAGRAPH / SUBHEADING / SUMMARY SECTION -->
           <template v-else>
             <div>
-              <label for="">Content:</label>
+              <label for="">Content</label>
               <textarea name="" id="" cols="30" rows="2"></textarea>
             </div>
           </template>
