2974c4a1a084eeefc18fb5ae7ef6163774bcf96b
Jana Wernick <jana.wernick@gmail.com>
2022-11-28 09:09:56 +0100

    feature: Tables can be added as sections

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,8 @@ const state = ref({
   ],
   uniqueSections: ['heading', 'summary'],
   previousSelect: '',
+  columnLimit: 4,
+  previousColumnName: '',
 });
 
 // GENERAL
@@ -185,8 +187,8 @@ function resetSectionAfterSelectChange(sectionName, sectionId) {
       articleForForm.value[i].items = [];
       break;
     case 'table':
-      // TODO Handle table case
-      console.log('%cresetSectionAfterSelectChange case table', 'color: darkseagreen; font-weight: bold;');
+      articleForForm.value[i].columns = [];
+      articleForForm.value[i].rows = [];
       break;
     default:
       articleForForm.value[i].value = '';
@@ -285,6 +287,222 @@ function selectListarrayInput(sectionId, indexItem) {
   }, 150);
 }
 
+// TABLE SECTION
+/**
+ * Adds a column to a table
+ * @param {number} sectionId The id of the table section
+ */
+function addTableColumn(sectionId) {
+  // Find index of table section with id
+  const indexTable = findArticleSectionIndex(sectionId);
+  // Check whether columns array already exists else create it
+  if (articleForForm.value[indexTable].columns === undefined) articleForForm.value[indexTable].columns = [];
+  // Disable button for adding columns if limit would be exceeded
+  if (articleForForm.value[indexTable].columns.length === state.value.columnLimit) {
+    disableTableColumnButton(true, sectionId);
+    return;
+  }
+  // Find out columns array length to set preliminary name for added column
+  const indexColumn = articleForForm.value[indexTable].columns.length;
+  // Push new empty object with name prop to table section with that id
+  articleForForm.value[indexTable].columns.push({
+      name: createProvisionalColumnName()
+  });
+  // Select added col name input for user
+  selectTableColumnInput(sectionId, indexColumn);
+  // TODO
+  // this.handleInputForShrunkSections(sectionId);
+}
+
+/**
+ * Delete a column name
+ * @param {number} sectionId The id of the table section
+ * @param {number} indexColumn The index of the column to be deleted
+ */
+function deleteTableColumn(sectionId, indexColumn) {
+  // Find index of table section with sectionId
+  const indexTable = findArticleSectionIndex(sectionId);
+  // Save columnName to pass as argument to deleteTableCells fn
+  const columnName = articleForForm.value[indexTable].columns[indexColumn].name;
+  // Delete column object in table columns
+  articleForForm.value[indexTable].columns.splice(indexColumn, 1);
+  // Delete respective cells from row that belong to column
+  deleteTableCells(indexTable, columnName);
+  // Enable button for adding columns
+  disableTableColumnButton(false, sectionId);
+  // Update table shrunk section
+  // this.handleInputForShrunkSections(sectionId);
+}
+
+/**
+ * Create a provisional name for an added column
+ */
+function createProvisionalColumnName() {
+  const randomString = Math.random().toString(36).substring(2, 6);
+  // E.g.: Column-7mqr
+  return `Column-${randomString}`;
+}
+
+/**
+ * Selects the newly added column
+ * @param {number} sectionId The id of the table section
+ * @param {number} indexColumn The index of the column to be added
+ */
+function selectTableColumnInput(sectionId, indexColumn) {
+  const selectTimeout = setTimeout(() => {
+    const tableColumnInput = document.querySelector(`#tableCol${indexColumn}-${sectionId}`);
+    tableColumnInput.focus();
+    tableColumnInput.select();
+    clearTimeout(selectTimeout);
+  }, 150);
+}
+
+/**
+ * Enable / disable the button that adds a column to a table
+ * @param {boolean} state The state to which the disabled attribute of the button should be set
+ * @param {number} sectionId The id of the table section
+ */
+function disableTableColumnButton(state, sectionId) {
+  const tableColumnButton = document.querySelector(`#tableColumnBtn-${sectionId}`);
+  tableColumnButton.disabled = state;
+}
+
+/**
+ * Saves the current column name on focus
+ * @param {string} columnName The column name of the column in focus
+ */
+function saveColumnName(columnName) {
+  if (columnName === '') return;
+  state.value.previousColumnName = columnName;
+}
+
+/**
+ * Handler thats manages column name input
+ * @param {object} e The blur event object
+ * @param {number} sectionId The id of the table section
+ * @param {number} indexColumn The index of the column
+ */
+function handleColumnInput(e, sectionId, indexColumn) {
+  // Set currently entered name
+  // NOTE: Since props are retrieved via string notation (obj['key']) for now, also characters that would be invalid inside identifiers can be used, like '€'
+  const currentColumnName = e.target.value;
+  // If no changes were made, return
+  if (state.value.previousColumnName === currentColumnName) return;
+
+  // Now it can be assumed that the user entered a column name that was not the previous
+  // Get table object id
+  const indexTable = findArticleSectionIndex(sectionId);
+  // Create array with only column name strings
+  const columnNames = articleForForm.value[indexTable].columns.map(({ name }) => name);
+  // Check columnNames for duplicate values
+  const duplicateColumnNames = containsDuplicateColumnNames(columnNames);
+
+  // If the user entered duplicate column name (one that is already occupied by another column) OR an empty string, then change it back to its provisional name, 'Column-1xyz'
+  if (duplicateColumnNames || currentColumnName === '') {
+    const provisonalColumnName = createProvisionalColumnName();
+    articleForForm.value[indexTable].columns[indexColumn].name = provisonalColumnName;
+
+    // In case of an empty column name
+    if (currentColumnName === '') {
+      // TODO
+      // Show error message to user
+      // articleForForm.value[indexTable].columns[indexColumn].errors = {};
+      // articleForForm.value[indexTable].columns[indexColumn].errors.missingColumnName = `A column name is required. A provisional name has been set for now. Please change it to your liking or delete the column.`;
+
+      // Clear error message after some time
+      // const errorTimeout = setTimeout(() => {
+      //     articleForForm.value[indexTable].columns[indexColumn].errors.missingColumnName = '';
+      //     clearTimeout(errorTimeout);
+      // }, 5000);
+    }
+
+    // Select added col name input for user
+    selectTableColumnInput(sectionId, indexColumn);
+    // Update row objects with provisional name
+    updateRowProps(provisonalColumnName, state.value.previousColumnName, indexTable);
+    // TODO
+    // Update table shrunk object
+    // this.handleInputForShrunkSections(sectionId);
+    return;
+  }
+
+  updateRowProps(currentColumnName, state.value.previousColumnName, indexTable);
+  // this.handleInputForShrunkSections(sectionId);
+}
+
+/**
+ * Returns true or false depending on whether duplicate column names exist
+ * @param {array} columnNames An array of string with all existing columns names of a table
+ * @returns {boolean} True if duplicate names exist, false if all column names are unique
+ */
+function containsDuplicateColumnNames(columnNames) {
+  return columnNames.some(columnName => {
+    // If a columnName is unique the index of its first + last occurence is the same, if not it's a duplicate
+    if (columnNames.indexOf(columnName) !== columnNames.lastIndexOf(columnName)) {
+        return true;
+    }
+    return false;
+  });
+}
+
+/**
+ * Adds a row to a table
+ * @param {number} sectionId The id of the table section
+ */
+ function addTableRow(sectionId) {
+  // Find index of table section with id
+  const indexTable = findArticleSectionIndex(sectionId);
+  // Check whether rows array already exists else create it
+  if (articleForForm.value[indexTable].rows === undefined) articleForForm.value[indexTable].rows = [];
+  // Push new empty object table section with that id (TODO row_id, is necessary?)
+  articleForForm.value[indexTable].rows.push({ });
+}
+
+/**
+ * Removes a row from a table
+ * @param {number} sectionId
+ * @param {number} indexRow
+ */
+function deleteTableRow(sectionId, indexRow) {
+  // Find index of table section with sectionId
+  const indexTable =  findArticleSectionIndex(sectionId);
+  // Delete row object in table rows
+  articleForForm.value[indexTable].rows.splice(indexRow, 1);
+}
+
+/**
+ * Updates a certain property of all row objects in the rows array of a table with the changed column name
+ * @param {string} newColumnName The new column name
+ * @param {string} previousColumnName The previous name that needs to be changed
+ * @param {number} indexTable The index of the table in the articlesForForm array
+ */
+function updateRowProps(newColumnName, previousColumnName, indexTable) {
+  // If rows array doesn't exist in table or is empty, return (previousColumnName prop does not exist either)
+  if (articleForForm.value[indexTable].rows === undefined || articleForForm.value[indexTable].rows.length === 0) return;
+
+  // If previousColumnName exists in first row of rows array in table, loop through each object and set the contents of previousColumnName to newColumnName + delete previousColumnName
+  if (articleForForm.value[indexTable].rows[0][previousColumnName]) {
+    articleForForm.value[indexTable].rows.forEach(row => {
+      row[newColumnName] = row[previousColumnName];
+      delete row[previousColumnName];
+    });
+  }
+}
+
+/**
+ * Deletes the properties in all row objects in the rows array of a table when the corresponding column name gets deleted
+ * @param {number} indexTable The index of the table in the articlesForForm array
+ * @param {string} columnName The name of the column that got deleted
+ */
+function deleteTableCells(indexTable, columnName) {
+  // If rows array doesn't exist in table or is empty, return
+  if (articleForForm.value[indexTable].rows === undefined || articleForForm.value[indexTable].rows.length === 0) return;
+  // Delete prop that is named after column in every object (row) of rows array
+  articleForForm.value[indexTable].rows.forEach(row => {
+      delete row[columnName];
+  });
+}
+
 // Create deep template copy
 // NOTE: Can be skipped if template logic will not be implemented
 const articleForForm = ref(JSON.parse(JSON.stringify(state.value.templateBasic)));
@@ -380,10 +598,8 @@ disableSectionsBasedOnArticle();
                 <div>
                   <!-- Loop through items -->
                   <div v-for="(item, i) in element.items" :key="`${element.name}Items-${element.id}`">
-                    <div>
-                      <label :for="`${element.name}Items-${element.id}-Item${i}`">{{ i + 1 }}</label>
-                      <textarea v-model.trim="item.value" :name="`${element.name}Items-${element.id}-Item${i}`" :id="`${element.name}Items-${element.id}-Item${i}`" cols="30" rows="1"></textarea>
-                    </div>
+                    <label :for="`${element.name}Items-${element.id}-Item${i}`">{{ i + 1 }}</label>
+                    <textarea v-model.trim="item.value" :name="`${element.name}Items-${element.id}-Item${i}`" :id="`${element.name}Items-${element.id}-Item${i}`" cols="30" rows="1"></textarea>
                     <button @click="deleteListArrayItem(element.id, i)" type="button">DEL</button>
                   </div>
                   <button @click="addListArrayItem(element.id)" type="button">+ Item</button>
@@ -394,8 +610,41 @@ disableSectionsBasedOnArticle();
 
           <!-- TABLE SECTION -->
           <template v-else-if="element.name === 'table'">
+            <!-- COLUMNS -->
             <div>
-              
+              <fieldset>
+                <legend>Columns</legend>
+                <div v-if="element.columns">
+                  <!-- Loop through columns -->
+                  <div v-for="(column, i) in element.columns"
+                    :key="`${element.name}Cols-${element.id}`">
+                    <label :for="`${element.name}Col${i}-${element.id}`">{{ i + 1 }}</label>
+                    <input v-model.trim="column.name" @focus="saveColumnName(column.name)" @blur="handleColumnInput($event, element.id, i)" type="text" :name="`${element.name}Col${i}-${element.id}`" :id="`${element.name}Col${i}-${element.id}`" :placeholder="`Column ${i + 1}`">
+                    <button @click="deleteTableColumn(element.id, i)" type="button">DEL</button>
+                  </div>
+                </div>
+                <button @click="addTableColumn(element.id)" type="button" :id="`${element.name}ColumnBtn-${element.id}`">+ Col</button>
+              </fieldset>
+            </div>
+
+            <!-- ROWS -->
+            <div>
+              <fieldset>
+                <legend>Rows</legend>
+                <div v-if="element.rows">
+                  <!-- Loop through rows -->
+                  <div v-for="(row, i) in element.rows" :key="`${element.name}Rows-${element.id}`">
+                    <label>Row {{ i + 1 }}</label>
+                    <!-- Loop through cells according to columns -->
+                    <div v-for="(cell, j) in element.columns">
+                      <label :for="`${element.name}Row${i}-Col${j}-${element.id}`"></label>
+                      <input v-model.trim="row[`${element.columns[j].name}`]" type="text" :name="`${element.name}Row${i}-Col${j}-${element.id}`" :id="`${element.name}Row${i}-Col${j}-${element.id}`" :placeholder="element.columns[j].name">
+                    </div>
+                    <button @click="deleteTableRow(element.id, i)" type="button">DEL</button>
+                  </div>
+                </div>
+                <button @click="addTableRow(element.id)" type="button">+ Row</button>
+              </fieldset>
             </div>
           </template>
 
